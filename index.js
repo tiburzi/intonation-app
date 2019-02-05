dec2bea88256ab594527ad11f44f6d63851bfe60
Jon <jontiburzi@gmail.com>
2019-02-04 23:16:23 -0600

    Got basic pitch recognition working!

---
+++ index.js
@@ -0,0 +1,53 @@
+const Pitchfinder = require("pitchfinder");
+const detectPitch = Pitchfinder.AMDF();
+var audioInputBuffer = null;
+
+// from https://developers.google.com/web/fundamentals/media/recording-audio/
+function initAudioBuffer() {
+	var handleSuccess = function(stream) {
+	    var context = new AudioContext();
+	    var source = context.createMediaStreamSource(stream);
+	    var processor = context.createScriptProcessor(1024, 1, 1);
+
+	    source.connect(processor);
+	    processor.connect(context.destination);
+
+	    processor.onaudioprocess = function(e) {
+	      // Do something with the data, i.e Convert this to WAV
+	      //console.log(e.inputBuffer);
+	      audioInputBuffer = e.inputBuffer;
+	    };
+	  };
+
+  navigator.mediaDevices.getUserMedia({ audio: true, video: false }).then(handleSuccess);
+}
+
+function getAudioBuffer() {
+	return audioInputBuffer;
+}
+
+function getPitch() {
+	//from https://github.com/peterkhayes/pitchfinder
+	const myAudioBuffer = getAudioBuffer(); // assume this returns a WebAudio AudioBuffer object
+	if (myAudioBuffer == null) {return null};
+	const float32Array = myAudioBuffer.getChannelData(0); // get a single channel of sound
+	return detectPitch(float32Array); // null if pitch cannot be identified
+}
+
+document.getElementById("test").innerHTML = "hello";
+
+initAudioBuffer();
+
+// Our main update loop!
+function update() {
+    // Keep track of time for time-synced animations and music
+    //updateTime();
+
+    var pitch = getPitch();
+    console.log(pitch);
+
+    // Ask the browser to run this on the next frame please   「 次のフラムをください。」
+    requestAnimationFrame( update );
+}
+
+update();
